709334ab647321cfd8ccbf4c67a7e0635eeabcf9
Jarrett Meyer <jarrettmeyer@gmail.com>
2012-05-08 21:15:37 -0400

    initial commit

---
+++ ForwardLib.Tests/Forwardable_Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Dynamic;
+using NUnit.Framework;
+
+namespace ForwardLib.Tests
+{
+    [TestFixture]
+    public class Forwardable_Tests
+    {
+        private TestObject testObject;
+        private dynamic testObjectPresenter;
+
+        [SetUp]
+        public void before_each_test()
+        {
+            testObject = new TestObject();
+            testObjectPresenter = new TestObjectPresenter(testObject);
+        }
+
+        [Test]
+        public void can_create_new_property()
+        {
+            Assert.AreEqual("0", testObjectPresenter.CastedIntValue);
+        }
+
+        [Test]
+        public void can_get_nested_int_property_value()
+        {
+            testObject.IntValue = 9;
+            Assert.AreEqual(9, testObjectPresenter.IntValue);
+        }
+
+        [Test]
+        public void can_get_nested_string_property_value()
+        {
+            testObject.StringValue = "Hello";
+            Assert.AreEqual("Hello", testObjectPresenter.StringValue);
+        }
+    }
+
+    public class TestObject
+    {
+        public int IntValue { get; set; }
+        public string StringValue { get; set; }
+    }
+
+    public class TestObjectPresenter : Forwardable<TestObject>
+    {
+        public TestObjectPresenter(TestObject testObject)
+            : base(testObject) { }
+
+        public string CastedIntValue
+        {
+            get
+            {
+                dynamic obj = this;
+                return obj.IntValue.ToString();
+            }
+        }
+    }
+}
---
+++ ForwardLib/Forwardable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ForwardLib
+{
+    public class Forwardable<T> : DynamicObject
+        where T : class
+    {
+        protected readonly dynamic instance;
+
+        protected Forwardable(dynamic instance)
+        {
+            if (instance == null)
+                throw new ArgumentNullException("instance");
+            this.instance = instance;
+        }
+
+        public virtual Type ForwardedType
+        {
+            get { return typeof(T); }
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            try
+            {
+                var memberName = binder.Name;
+                var propertyInfo = ForwardedType.GetProperty(memberName);
+                result = propertyInfo.GetValue(instance, null);
+                return true;
+            }
+            catch (Exception)
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+}
